7c3efd17ab13b14d6a9f665fc3336deafeae82b8
gorka <juno6pm@gmail.com>
2014-01-26 23:58:09 +0100

    implemented the whole rest interface. added commands to rest_interface.txt

--- linino/yun-radio.py
+++ linino/yun-radio.py
@@ -69,6 +69,10 @@ def stop_stream(proc):
 #    print(cmd)
     proc.stdin.write(cmd)
 
+def restart_stream(proc):
+    cmd = 'K 0\n'
+    proc.stdin.write(cmd)
+
 def set_volume(proc, val):
     cmd = 'V ' + val + '\n'
 #    print(cmd)
@@ -91,11 +95,25 @@ def random_iter(seq, start):
     first_item_index = shuffled.index(first_item)
     shuffled[first_item_index] = shuffled[0]
     shuffled[0] = first_item
-    return iter(shuffled)
+    return shuffled
 
 def reorder_iter(seq, start):
     reorder = list(seq)
-    return iter(reorder[start:len(reorder)]+reorder[0:start])
+    return reorder[start:len(reorder)]+reorder[0:start]
+
+
+
+
+
+
+
+
+
+
+users_array = []
+playlist_array = []
+radios_array = []
+shuffle = 0
 
 
 
@@ -123,6 +141,7 @@ try:
     for line in users:
         user_elements = line.split()
         print(line)
+        users_array.append(user_elements[0])
         for song in client.collection(user_elements[0]):
 #           print(unicode(song.name).encode("utf-8"))
             rest_interface_put('user' + str(nusers) + 'song' + str(nsongs),  (unicode(song.name).encode("utf-8")))
@@ -148,6 +167,7 @@ try:
     playlist = open('gs_playlists', 'r+')
 
     for line in playlist:
+        playlist_array.append(line)
 #        print(client.playlist(line).name)
         for song in client.playlist(line).songs:
 #           print(unicode(song.name).encode("utf-8"))
@@ -174,10 +194,10 @@ try:
 
     for line in radios:
         radio_element = line.split()
+        radios_array.append(radio_element[0])
 #       print(line)
         rest_interface_put('radio' + str(nradios) + 'name', (unicode(radio_element[1]).encode("utf-8")))
         nradios+=1
-        radio = radio_element[0]
 except IOError:
     pass
 
@@ -199,38 +219,129 @@ mpg123_proc = subprocess.Popen(['mpg123', '-R'], stdin=subprocess.PIPE, stdout=s
 
 rest_interface_put('lininoStatus', 'Ready')
 
-#song_iter = random_iter(client.playlist(94228635).songs, 3)
-song_iter = reorder_iter(client.playlist(94228635).songs, 1)
+audio_iter = None
+prev_pos = -1
+element = 0
 
-#for song in client.radio(radio):
-for song in song_iter:
-    prev_pos = -1
-    load_stream(mpg123_proc, song)
+current_pos = 0
+song_duration = 0
 
-    while mpg123_proc.stdout:
-        readable = select.select([mpg123_proc.stdout], [], [], 1)[0]
-        for s in readable:
-            mpg123_line = mpg123_proc.stdout.readline() 
-#            print(mpg123_line)
-            eos = mpg123_line.find('@P 0')
-            pos = mpg123_line.find('@F')
-            
-            if (pos != -1) :
-                current_pos = int(float(mpg123_line.split()[3]))
-                if song.duration == 0:
-                    song_duration = current_pos + int(float(mpg123_line.split()[4]))
-                else:
-                    song_duration = song.duration                    
+song = None
+playingRadio = 0
+repeat = 0
+
+while 1:
+    command = rest_interface_get('command')
+    if command != 'none':
+        if command == 'playRadio\n':
+            radio = rest_interface_get('commandData1')
+            audio_iter = client.radio(radios_array[int(radio)]).__iter__()
+            playingRadio = 1
+            stop_stream(mpg123_proc)
+
+        elif command == 'playUser\n':
+            playingRadio = 0
+            element = 0
+            user = rest_interface_get('commandData1')
+            user_song = rest_interface_get('commandData2')
+            audio_iter = client.collection(users_array[int(user)])
+            if shuffle:
+                audio_iter = random_iter(audio_iter, int(user_song))
+            else:
+                audio_iter = reorder_iter(audio_iter, int(user_song))
+            print("len       " + str(len(audio_iter)))
+            stop_stream(mpg123_proc)
+
+        elif command == 'playPlayList\n':
+            playingRadio = 0
+            element = 0
+            playlist = rest_interface_get('commandData1')
+            playlist_song = rest_interface_get('commandData2')
+            audio_iter = client.playlist(playlist_array[int(user)]).songs
+            if shuffle:
+                audio_iter = random_iter(audio_iter, int(user_song))
+            else:
+                audio_iter = reorder_iter(audio_iter, int(user_song))
+            stop_stream(mpg123_proc)
+
+        elif command == 'shuffle\n':
+            shuffle_str = rest_interface_get('commandData1')
+            if shuffle_str == 'enable\n':
+                shuffle = 1
+            else:
+                shuffle = 0
+        
+        elif command == 'volume\n':
+            vol = rest_interface_get('commandData1')
+            set_volume(mpg123_proc, int(vol))
+
+        elif command == 'pause\n':
+            pause_stream(mpg123_proc)
+        
+        elif command == 'next\n':
+            stop_stream(mpg123_proc)
+
+        elif command == 'prev\n':
+            if playingRadio :
+                repeat = 1
+            else:
+                if element > 1:
+                    element-=2
+                elif element > 0:
+                    element -= 1
+            stop_stream(mpg123_proc)
+
+        elif command == 'restart\n':
+            if playingRadio :
+                repeat = 1
+            else:
+                if element > 0:
+                    element-=1
+            stop_stream(mpg123_proc)
+
+
+    readable = select.select([mpg123_proc.stdout], [], [], 0.25)[0]
+    for s in readable:
+        mpg123_line = mpg123_proc.stdout.readline() 
+#        print(mpg123_line)
+        eos = mpg123_line.find('@P 0')
+        pos = mpg123_line.find('@F')
+        
+        if (pos != -1) :
+            current_pos = int(float(mpg123_line.split()[3]))
+            if song.duration == 0 or song.duration == None:
+                song_duration = current_pos + int(float(mpg123_line.split()[4]))
+            else:
+                song_duration = song.duration                    
 #                print(current_pos + " - " + str(song.duration))
 
-            if (eos != -1) :
-                break
+        if (eos != -1) :
+            if audio_iter != None:
+                try:
+                    if playingRadio :
+                        if repeat:
+                            repeat = 0
+                        else:
+                            song = audio_iter.next()
+                    else:
+                        print("element   " + str(element))
+                        print("len       " + str(len(audio_iter)))
+                        song = audio_iter[element]
+                    load_stream(mpg123_proc, song)
+                    prev_pos = -1
+                    current_pos = 0
+                    song_duration = 0
+                    element += 1
+                except StopIteration:
+                    print("End of playback")
+            break
 # This is to disable playback, so that we don't have to wait for the full audio to play
 #        stop_stream(mpg123_proc)
-        if prev_pos != current_pos :   
+    if prev_pos != current_pos :   
 #            print(str(current_pos) + " - " + str(song_duration))
-            rest_interface_put('nowPlaying_pos', str(current_pos))
-            rest_interface_put('nowPlaying_dur', str(song_duration))
-            prev_pos = current_pos
-        if eos!=-1 :
-            break;
+        rest_interface_put('nowPlaying_pos', str(current_pos))
+        rest_interface_put('nowPlaying_dur', str(song_duration))
+        prev_pos = current_pos
+
+
+
